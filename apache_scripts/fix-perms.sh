1e7df4bdd0a7e9265586e6424569617695ccd5cd
Robinson Lemos <robilemos89@gmail.com>
2017-04-30 12:28:11 -0300

    Corrigido alguns detalhes no script de arrumar as permissões

--- apache_scripts/fix-perms.sh
+++ apache_scripts/fix-perms.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-if [ $# -gt 1 ]; then
-    sudo setfacl -r -m g:www-data:rwx $1
+
+if [ $# = 1 ]; then
+    sudo setfacl -R -m g:www-data:rwx $1
+    sudo chown `whoami`:`whoami` $1
+    sudo chmod -R 775 $1
     sudo chmod -R g+s $1
     sudo chmod -R u+s $1
 fi
